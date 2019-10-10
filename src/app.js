9aa49322416a222ffe022a32125a29622d9cc693
Monika <monikahoex@gmail.com>
2019-10-09 22:20:41 -0700

    Set up React Router

--- src/app.js
+++ src/app.js
@@ -2,11 +2,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
-
-// Components
-import IndecisionApp from './components/IndecisionApp';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 // Sass
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const ExpensiftyDashboardPage = () => <p>Expensify Dashboard</p>;
+const AddExpensePage = () => <p>Add Expense</p>;
+const EditExpensePage = () => <p>Edit Expense</p>;
+const HelpPage = () => <p>Help page</p>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpensiftyDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
