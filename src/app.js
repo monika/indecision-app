e35d75265bba4f60d17232e09f712cbeaec47134
Monika <monikahoex@gmail.com>
2019-10-09 23:32:42 -0700

    Revert "Set up React Router"
    This reverts commit 9aa49322416a222ffe022a32125a29622d9cc693.

--- src/app.js
+++ src/app.js
@@ -2,25 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+
+// Components
+import IndecisionApp from './components/IndecisionApp';
 
 // Sass
 import './styles/styles.scss';
 
-const ExpensiftyDashboardPage = () => <p>Expensify Dashboard</p>;
-const AddExpensePage = () => <p>Add Expense</p>;
-const EditExpensePage = () => <p>Edit Expense</p>;
-const HelpPage = () => <p>Help page</p>;
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Route path="/" component={ExpensiftyDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
